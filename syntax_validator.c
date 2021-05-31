4ac6ee3b9c8eed6b40cb6581e07b222bf005e6cb
Lajuana Bespin <lbespin@pr-e3.kzn.21-school.ru>
2021-05-31 14:40:13 +0300

    fix syntax validator, validate quotes, slash, operators

--- syntax_validator.c
+++ syntax_validator.c
@@ -6,31 +6,79 @@
 #define BACKSLASH '\\'
 #define DOLOR '$'
 
-static BOOLEAN		is_correct_quotes(t_vector *expression, char quote)
+static BOOLEAN	is_correct_quote(t_vector *expression, char quote)
 {
 	char	sym;
 	char	prev_sym;
-	BOOLEAN	in_quotes;
-	
-	expression->pos = 0;
+
 	prev_sym = 0;
-	in_quotes = FALSE;
 	while (has_next(expression))
 	{
 		sym = *(char *)next(expression);
-		if (sym == quote && sym != BACKSLASH)
-		{
-			if (in_quotes == FALSE)
-				in_quotes = TRUE;
-			else
-				in_quotes = FALSE;
-		}
+		if (sym == quote && prev_sym != BACKSLASH)
+			return (TRUE);
 		if (sym == BACKSLASH && prev_sym == BACKSLASH)
 			prev_sym = 0;
 		else
 			prev_sym = sym;
 	}
-	return (!in_quotes);
+	return (FALSE);
+}
+
+static BOOLEAN	check_operators(t_vector *expression, char sym)
+{
+	t_vector	*op;
+	BOOLEAN		is_correct;
+
+	op = new_vector(CHAR);
+	if (!op)
+		ft_eprintf("malloc check operators");
+	op->method->push_back(op, &sym);
+	while (has_next(expression))
+	{
+		sym = *(char *)next(expression);
+		if (sym == ' ')
+			continue ;
+		if (ft_strchr("><|;", sym))
+			op->method->push_back(op, &sym);
+		else
+			break ;
+	}
+	if (has_next(expression))
+		previous(expression);
+	if (has_next(expression) && (op->size == 1 || !ft_strcmp(op->mem, ">>")))
+		is_correct = TRUE;
+	else
+		is_correct = FALSE;
+	delete(op);
+	return (is_correct);
+}
+
+static BOOLEAN	check_quotes(t_vector *expression)
+{
+	BOOLEAN	is_correct;
+	char	sym;
+	char	prev_sym;
+
+	is_correct = TRUE;
+	expression->pos = 0;
+	sym = 0;
+	while (has_next(expression))
+	{
+		prev_sym = sym;
+		sym = *(char *)next(expression);
+		if (sym == DOUBLEQUOTE && prev_sym != BACKSLASH)
+			is_correct &= is_correct_quote(expression, DOUBLEQUOTE);
+		else if (sym == SINGLEQUOTE && prev_sym != BACKSLASH)
+			is_correct &= is_correct_quote(expression, SINGLEQUOTE);
+		else if (ft_strchr("><|;", sym) && prev_sym != BACKSLASH)
+			is_correct &= check_operators(expression, sym);
+		else if (sym == BACKSLASH && prev_sym == BACKSLASH)
+			sym = 0;
+	}
+	if (sym == BACKSLASH)
+		return (FALSE);
+	return (is_correct);
 }
 
 BOOLEAN	is_correct_syntax(t_vector *expression)
@@ -38,7 +86,8 @@ BOOLEAN	is_correct_syntax(t_vector *expression)
 	BOOLEAN	is_correct;
 
 	is_correct = TRUE;
-	is_correct &= is_correct_quotes(expression, DOUBLEQUOTE);
-	is_correct &= is_correct_quotes(expression, SINGLEQUOTE);
+	is_correct &= check_quotes(expression);
+	if (!is_correct)
+		ft_printf("%s: syntax error\n", ft_getprogname());
 	return (is_correct);
 }
