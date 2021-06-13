c5202c6fcf61efae9c35c7c2e966d29aff99b0c6
Thunderbird Phung <tphung@pr-i2.kzn.21-school.ru>
2021-06-13 15:47:02 +0300

    ft_cd is fine now

--- launcher/builtins/ft_cd.c
+++ launcher/builtins/ft_cd.c
@@ -6,36 +6,86 @@
 /*   By: tphung <tphung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:50:25 by tphung            #+#    #+#             */
-/*   Updated: 2021/06/12 20:22:35 by tphung           ###   ########.fr       */
+/*   Updated: 2021/06/13 15:45:16 by tphung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/inc.h"
 #include "../includes/utils.h"
 
+char	*envp_var_eject(t_vector *envp, int src_pos)
+{
+	char	*res;
+
+	res = *(char **)envp->method->at(envp, src_pos);
+	res = ft_substr(res, \
+		1 + ft_strchr(res, '=') - res, ft_strlen(res));
+	return (res);
+}
+
+int	var_export(t_vector *envp, int dest_pos, char *var, char *dest_var_name)
+{
+	char		*dest;
+
+	printf("VAR = %s\n", var);
+	dest = ft_strjoin(dest_var_name, var);
+	printf("NEW = %s\n", dest);
+	free(*(char **)envp->method->at(envp, dest_pos));
+	envp->method->erase(envp, dest_pos);
+	envp->method->insert(envp, &dest, dest_pos);
+	return (0);
+}
+
+int	pwd_export(t_vector *envp, char *old_pwd)
+{
+	int		pos_pwd;
+	int		pos_old;
+	char	*var;
+
+	pos_pwd = locate_env("PWD", envp);
+	pos_old = locate_env("OLDPWD", envp);
+	if (pos_old >= 0 && pos_pwd >= 0)
+	{
+		var = envp_var_eject(envp, pos_pwd);
+		var_export(envp, pos_old, var, "OLDPWD=");
+		free(var);
+	}
+	else if (pos_old >= 0)
+		var_export(envp, pos_old, old_pwd, "OLDPWD=");
+	if (pos_pwd >= 0)
+	{
+		var = getcwd(NULL, 0);
+		var_export(envp, pos_pwd, var, "PWD=");
+		free(var);
+	}
+	return (0);
+}
+
 int	ft_cd(char **args, t_vector *envp)
 {
-	int	pos;
+	int		pos;
 	char	*dir;
-	char	*old_dir;
+	char	*pwd;
 
-	pos = locate_env("HOME", envp);
+	pwd = getcwd(NULL, 0);
 	if (!args || !args[1])
 	{
+		pos = locate_env("HOME", envp);
 		if (pos < 0)
 		{
 			ft_fprintf(2, "minishell: cd: HOME not set\n");
 			return (0);
 		}
-		dir = *(char**)envp->method->at(envp, pos);
-		dir = ft_substr(dir,\
-			1 + ft_strchr(dir, '=') - dir, ft_strlen(dir));
+		dir = envp_var_eject(envp, pos);
 	}
 	else
 		dir = ft_strdup(args[1]);
 	chdir(dir);
-	free(dir);
 	if (errno > 0)
 		ft_wprintf("cd");
+	else
+		pwd_export(envp, pwd);
+	free(dir);
+	free(pwd);
 	return (0);
 }
