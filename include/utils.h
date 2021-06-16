0e7df100055721d3180984ca562f97bda84f89d9
Thunderbird Phung <tphung@pr-k4.kzn.21-school.ru>
2021-06-16 18:11:00 +0300

    norm in Headers

--- include/utils.h
+++ include/utils.h
@@ -3,15 +3,13 @@
 
 char	*filename_parser(char *filename, char **envp);
 pid_t	launcher(t_main *arg, t_vector *envp);
-
-int	locate_env(char *var, t_vector *envp);
-int fd_replacement(int old_fd, int new_fd);
-
-int	ft_cd(char **args, t_vector *envp);
-int	ft_pwd(void);
-int ft_export(char **argv, t_vector *envp);
-int	ft_env(t_vector *envp);
-int	ft_echo(char **args);
-int ft_unset(char **argv, t_vector *envp);
+int		locate_env(char *var, t_vector *envp);
+int		fd_replacement(int old_fd, int new_fd);
+int		ft_cd(char **args, t_vector *envp);
+int		ft_pwd(void);
+int		ft_export(char **argv, t_vector *envp);
+int		ft_env(t_vector *envp);
+int		ft_echo(char **args);
+int		ft_unset(char **argv, t_vector *envp);
 
 #endif
